781c6dbb62efc6e5a8d7ad53fb167b0e1bd94ad6
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-10 19:48:32 -0400

    Added new custom type and docstrings.

--- tvmaze/types.ts
+++ tvmaze/types.ts
@@ -6,12 +6,22 @@ type tShowInfo = {
   image: string,
 };
 
+/** Type of Object that holds score and show information. */
 type tShowInfoAndScore = {
   score: number,
   show: tShowInfo & {image: {medium: string, original: string} | undefined },
 };
 
+/** Type of Object that holds information about a show's episode. */
+type tEpisodeInfo = {
+  id: number,
+  name: string,
+  season: number,
+  number: number
+}
+
 export type {
   tShowInfo,
   tShowInfoAndScore,
+  tEpisodeInfo
 };
