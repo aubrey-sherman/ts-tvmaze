194ad51cdcd46ad1c6d1bc2db0cef1ac9952eee3
Anya Aven <leafeon96@gmail.com>
2024-06-10 20:57:01 -0400

    Added type of HTTPMethods

--- tvmaze/types.ts
+++ tvmaze/types.ts
@@ -6,10 +6,15 @@ type tShowInfo = {
   image: string,
 };
 
-/** Type of Object that holds score and show information. */
+/** Type of Object that holds score and show information.
+ * Data coming from the API of TV Maze
+*/
 type tShowInfoAndScore = {
   score: number,
-  show: tShowInfo & {image: {medium: string, original: string} | undefined },
+  show: tShowInfo & {
+    image: { medium: string, original: string; } |
+    undefined;
+  },
 };
 
 /** Type of Object that holds information about a show's episode. */
@@ -17,11 +22,23 @@ type tEpisodeInfo = {
   id: number,
   name: string,
   season: number,
-  number: number
-}
+  number: number;
+};
+
+/** Valid HTTP methods */
+type tHTTPMethods =
+  "all" |
+  "head" |
+  "get" |
+  "post" |
+  "put" |
+  "patch" |
+  "delete" |
+  "options";
 
 export type {
   tShowInfo,
   tShowInfoAndScore,
-  tEpisodeInfo
+  tEpisodeInfo,
+  tHTTPMethods,
 };
